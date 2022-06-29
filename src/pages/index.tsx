865e4a6efa4ead6ff22cdd7e1f878e569f72fc4e
opatricpires <patric.pires@outlook.com>
2022-06-29 14:44:51 -0300

    chore: start project and remove unused libs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,6 @@ import Main from 'components/Main'
 
 export default function Home() {
   return (
-    <Main
-      title="React Avançado"
-      description="TypseScript, ReactJS, NextJS e Styled Component"
-    />
+   <h1></h1>
   )
 }
